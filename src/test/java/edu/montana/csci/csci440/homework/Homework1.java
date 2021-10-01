4ea28fef3d926560888f3f66fa2b34c98574291c
codephil <philip@meiers.in>
2021-09-30 19:41:40 -0600

    hw1

--- src/test/java/edu/montana/csci/csci440/homework/Homework1.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework1.java
@@ -11,9 +11,10 @@ public class Homework1 extends DBTest {
 
     @Test
     /*
-     * Write a query in the string below that returns all artists that have an 'A' in their name
+     * Write a query in the string below that returns all artists that have an 'A'
+     * in their name
      */
-    void selectArtistsWhoseNameHasAnAInIt(){
+    void selectArtistsWhoseNameHasAnAInIt() {
         List<Map<String, Object>> results = executeSQL("SELECT * FROM artists");
         assertEquals(211, results.size());
     }
@@ -28,17 +29,18 @@ public class Homework1 extends DBTest {
 
         assertEquals(56, results.size());
         assertEquals("AC/DC", results.get(0).get("Name"));
+        print('Holla!');
     }
 
     @Test
-        /*
-         * Write a query in the string below that returns all tracks longer than six minutes along with the
-         * album and artist name
-         */
+    /*
+     * Write a query in the string below that returns all tracks longer than six
+     * minutes along with the album and artist name
+     */
     void selectTheTrackAndAlbumAndArtistForAllTracksLongerThanSixMinutes() {
         List<Map<String, Object>> results = executeSQL(
-                "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks " +
-                        "-- NEED TO DO SOME JOINS HERE KIDS");
+                "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks "
+                        + "-- NEED TO DO SOME JOINS HERE KIDS");
 
         assertEquals(623, results.size());
 
@@ -46,13 +48,13 @@ public class Homework1 extends DBTest {
         // to ORDER BY
         //
         //
-//        assertEquals("Princess of the Dawn", results.get(0).get("TrackName"));
-//        assertEquals("Restless and Wild", results.get(0).get("AlbumTitle"));
-//        assertEquals("Accept", results.get(0).get("ArtistsName"));
-//
-//        assertEquals("Snoopy's search-Red baron", results.get(10).get("TrackName"));
-//        assertEquals("The Best Of Billy Cobham", results.get(10).get("AlbumTitle"));
-//        assertEquals("Billy Cobham", results.get(10).get("ArtistsName"));
+        // assertEquals("Princess of the Dawn", results.get(0).get("TrackName"));
+        // assertEquals("Restless and Wild", results.get(0).get("AlbumTitle"));
+        // assertEquals("Accept", results.get(0).get("ArtistsName"));
+        //
+        // assertEquals("Snoopy's search-Red baron", results.get(10).get("TrackName"));
+        // assertEquals("The Best Of Billy Cobham", results.get(10).get("AlbumTitle"));
+        // assertEquals("Billy Cobham", results.get(10).get("ArtistsName"));
 
     }
 
